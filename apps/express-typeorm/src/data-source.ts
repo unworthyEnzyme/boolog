ebac1b69cc8e6eb43e778e3064129e7d8d757aee
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-11-15 23:38:08 +0300

    disable logging

--- apps/express-typeorm/src/data-source.ts
+++ apps/express-typeorm/src/data-source.ts
@@ -3,7 +3,6 @@ import { User } from "typeorm-entities";
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
-  logging: true,
   database: "db.db",
   entities: [User],
   synchronize: true,
