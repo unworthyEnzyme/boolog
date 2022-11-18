5aa8f25eefd30d5af2c23daf42e6210ba7bcae5e
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-11-18 21:39:13 +0300

    implement session

--- apps/express-typeorm/src/routes/auth.ts
+++ apps/express-typeorm/src/routes/auth.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { SignupRequestSchema, LoginRequestSchema } from "types-on-wire";
-import { AuthSession } from "mongo-session";
+import { createSession } from "mongo-session";
 import { User } from "typeorm-entities";
 import argon2 from "argon2";
+import DeferredPromise from "deferred-promise";
 
 const router = Router();
 
@@ -22,17 +23,46 @@ router.post("/signup", async (req, res) => {
       return user;
     })
     .then((v) => v.save())
+    .then(async (v) => {
+      const session = await createSession(v.username).catch((e) => null);
+      if (!session) return;
+      res.set(
+        "Set-Cookie",
+        `sessionId=${
+          session.id
+        }; Expires=${session.expirationDate.toISOString()}; HttpOnly; Secure; Path=/`
+      );
+    })
     .then(() => res.sendStatus(201))
     .catch((e) => res.sendStatus(400));
 });
 
 router.post("/login", async (req, res) => {
+  const username = new DeferredPromise<string>();
   LoginRequestSchema.parseAsync(req.body)
     .then(async (v) => {
       const user = await User.findOneBy({ username: v.username });
       return { user, reqPassword: v.password };
     })
-    .then((v) => argon2.verify(v.user?.password ?? "$", v.reqPassword))
+    .then(async (v) => {
+      const verified = await argon2.verify(
+        v.user?.password ?? "$",
+        v.reqPassword
+      );
+      username.resolve(v.user!.username);
+      return verified;
+    })
+    .then(async (v) => {
+      const session = await createSession((await username) as string).catch();
+      if (!session) return v;
+      res.set(
+        "Set-Cookie",
+        `sessionId=${
+          session.id
+        }; Expires=${session.expirationDate.toISOString()}; HttpOnly; Secure; Path=/`
+      );
+      return v;
+    })
     .then((v) => res.sendStatus(v ? 200 : 400))
     .catch((e) => res.sendStatus(400));
 });
--- packages/mongo-session/src/index.ts
+++ packages/mongo-session/src/index.ts
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import crypto from "crypto";
+import * as fns from "date-fns";
 const SECOND = 1;
 const MINUTE = SECOND * 60;
 const HOUR = MINUTE * 60;
@@ -28,4 +30,20 @@ export async function connect() {
 
 export async function disconnect() {
   await mongoose.disconnect();
+}
+
+export async function createSession(username: string) {
+  const id = crypto.randomBytes(16).toString("base64");
+  const expirationDate = fns.add(new Date(), { days: 1 });
+  const session = await AuthSession.create({ id, username });
+  await session.save();
+  return { id, expirationDate };
+}
+
+export async function getSessionById(id: string) {
+  return await AuthSession.findOne({ id });
+}
+
+export async function getSessions(username: string) {
+  return await AuthSession.find({ username });
 }
