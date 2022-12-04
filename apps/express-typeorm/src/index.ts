d6b7bb778365dafcf9317470ae19257062dcfaad
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-12-04 18:23:00 +0300

    create a blog route.

--- apps/express-typeorm/src/index.ts
+++ apps/express-typeorm/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { AppDataSource } from "./data-source";
 import { connect } from "mongo-session";
 import authRoute from "./routes/auth";
+import blogRouter from "./routes/blog";
 import cookieParser from "cookie-parser";
 
 const main = async () => {
@@ -9,6 +10,7 @@ const main = async () => {
   app.use(express.json());
   app.use(cookieParser());
   app.use("/auth", authRoute);
+  app.use("/blogs", blogRouter);
   app.get("/", (_, res) => res.json({ hello: "world" }));
   await AppDataSource.initialize();
   await connect();
---
+++ apps/express-typeorm/src/routes/blog.ts
@@ -0,0 +1,47 @@
+import { Router } from "express";
+import { Blog, User } from "typeorm-entities";
+import { CreateBlogRequestSchema } from "types-on-wire";
+import { authenticated } from "../middlewares";
+
+const router = Router();
+
+router.get("/:id", (req, res) => {
+  Blog.findOneBy({ id: +req.params.id })
+    .then((v) => (v === null ? res.sendStatus(404) : res.json(v)))
+    .catch((e) => {
+      console.error(e);
+      res.sendStatus(500);
+    });
+});
+
+router.get("/", (req, res) => {
+  Blog.find()
+    .then((v) => res.json(v))
+    .catch((e) => {
+      console.error(e);
+      res.sendStatus(500);
+    });
+});
+
+router.post("/", authenticated, (req, res) => {
+  CreateBlogRequestSchema.parseAsync(req.body)
+    .then(({ content }) => {
+      const blog = new Blog();
+      blog.content = content;
+      return blog;
+    })
+    .then((blog) => {
+      //@ts-ignore
+      const user = req.user as User;
+      blog.author = user;
+      return blog.save();
+    })
+    .then((saved) => {
+      res.json({ id: saved.id });
+    })
+    .catch((e: Error) => {
+      res.sendStatus(400);
+    });
+});
+
+export default router;
