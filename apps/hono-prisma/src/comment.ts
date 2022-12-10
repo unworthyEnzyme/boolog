d5ec1d5b503952143adb8eef95a71d4b1ccc7c0b
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-12-10 17:45:07 +0300

    comment router

---
+++ apps/hono-prisma/src/comment.ts
@@ -0,0 +1,76 @@
+import { Hono } from "hono";
+import { validator } from "hono/validator";
+import { client } from "./prisma";
+
+export const commentRouter = new Hono();
+
+commentRouter.get("/:id", async (c) => {
+  const comment = await client.comment.findUnique({
+    where: { id: +c.req.param("id") },
+    select: {
+      id: true,
+      content: true,
+      user: { select: { username: true } },
+    },
+  });
+  if (!comment) return c.body(null, 404);
+  return c.json(comment);
+});
+
+commentRouter.post(
+  "/",
+  validator((v) => ({
+    content: v.json("content").isRequired().message("content is required"),
+    blogId: v
+      .json("blogId")
+      .isNumeric()
+      .isRequired()
+      .message("`blogId` is required"),
+  })),
+  async (c) => {
+    const username = c.req.headers.get("x-username")!;
+    const user = await client.user.findUnique({ where: { username } });
+    if (!user) return c.body(null, 401);
+    const body = c.req.valid();
+    const comment = await client.comment.create({
+      data: { content: body.content, userId: user.id, blogId: +body.blogId },
+    });
+    return c.json({ id: comment.id }, 201);
+  }
+);
+
+commentRouter.delete("/:id", async (c) => {
+  const username = c.req.headers.get("x-username")!;
+  const user = await client.user.findUnique({ where: { username } });
+  if (!user) return c.body(null, 401);
+  const comment = await client.comment.findUnique({
+    where: { id: +c.req.param("id") },
+  });
+  if (!comment) return c.body(null, 400);
+  if (comment.userId !== user.id) return c.body(null, 401);
+  await client.comment.delete({ where: { id: comment.id } });
+  return c.json({ id: comment.id });
+});
+
+commentRouter.patch(
+  "/:id",
+  validator((v) => ({
+    content: v.json("content").isRequired(),
+  })),
+  async (c) => {
+    const username = c.req.headers.get("x-username")!;
+    const user = await client.user.findUnique({ where: { username } });
+    if (!user) return c.body(null, 401);
+    const comment = await client.comment.findUnique({
+      where: { id: +c.req.param("id") },
+    });
+    if (!comment) return c.body(null, 400);
+    if (comment.userId !== user.id) return c.body(null, 401);
+    const { content } = c.req.valid();
+    await client.comment.update({
+      where: { id: comment.id },
+      data: { content },
+    });
+    return c.json({ content });
+  }
+);
--- apps/hono-prisma/src/server.ts
+++ apps/hono-prisma/src/server.ts
@@ -6,6 +6,7 @@ import { client as redisClient } from "./redis";
 import { client as prismaClient } from "./prisma";
 import { authenticated } from "./session";
 import { blogRouter } from "./blog";
+import { commentRouter } from "./comment";
 
 const app = new Hono();
 
@@ -14,6 +15,7 @@ app.use("*", authenticated);
 app.get("/api", (c) => c.text("Hono meets Node.js"));
 app.route("/api/auth", authRouter);
 app.route("/api/blogs", blogRouter);
+app.route("/api/comments", commentRouter);
 
 app.onError((e, c) => {
   c.status(400);
