e89089f5835b1512621313547caa201e07fb3ea5
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-12-11 17:00:19 +0300

    configure swr

--- apps/react-spa/src/App.tsx
+++ apps/react-spa/src/App.tsx
@@ -1,5 +1,13 @@
+import { SWRConfig } from "swr";
+
 function App() {
-  return <div className="text-red-500">hello world</div>;
+  return (
+    <SWRConfig value={{ suspense: true }}>
+      <div className="text-red-400 bg-slate-300 w-12 rounded-lg">
+        hello world
+      </div>
+    </SWRConfig>
+  );
 }
 
 export default App;
