406fd6343e7e4a15a1b5b9406916bcac7f4c4e5c
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-12-11 21:48:01 +0300

    should send the cookies

--- apps/react-spa/src/components/Protected.tsx
+++ apps/react-spa/src/components/Protected.tsx
@@ -2,7 +2,7 @@ import useSwr from "swr";
 
 export function Protected({ children }: { children: JSX.Element }) {
   useSwr("http://localhost:3000/api/auth/me", async (url) => {
-    const res = await fetch(url);
+    const res = await fetch(url, { credentials: "include" });
     if (res.status === 401) throw new Error("Not Authorized");
     return await res.json();
   });
