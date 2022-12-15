4ed8155e367adeb4f9170fb3f90864537221351f
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-12-15 14:02:32 +0300

    I am not sure if i want't to deal with errors here but whatever

--- apps/react-spa/src/router.tsx
+++ apps/react-spa/src/router.tsx
@@ -2,15 +2,23 @@ import { createBrowserRouter, Navigate } from "react-router-dom";
 import { Login } from "./components/Login";
 import { Protected } from "./components/Protected";
 import { Signup } from "./components/Signup";
+import { HomePage } from "./components/HomePage";
 import { ErrorBoundary } from "react-error-boundary";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: (
-      <ErrorBoundary fallback={<Navigate to="/login" />}>
+      <ErrorBoundary
+        fallbackRender={({ error }) => {
+          if (error.message === "Not Authorized") {
+            return <Navigate to="/login" relative="route"></Navigate>;
+          }
+          return <div>{error.message}</div>;
+        }}
+      >
         <Protected>
-          <div>hello world</div>
+          <HomePage />
         </Protected>
       </ErrorBoundary>
     ),
